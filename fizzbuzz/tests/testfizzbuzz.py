7ecd1d814b3702ba8e96f705608eebc9c7d1250a
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-03 19:45:14 +0700

    update test case 1 pass

--- fizzbuzz/tests/testfizzbuzz.py
+++ fizzbuzz/tests/testfizzbuzz.py
@@ -1,7 +1,8 @@
 from fizzbuzz.fizzbuzzcode.fizzbuzz_d import FizzBuzz
 import unittest
 class PrimeListTest(unittest.TestCase):
-    def test_give_1_2_3_is_prime(self):
-        FB_list = [1, 2, 3]
-        is_FB = FizzBuzz(FB_list)
-        self.assertTrue(is_FB)
+    def test_give_1_2_3 (self):
+        numbers = [1, 2, 3]
+        are_fb = FizzBuzz(numbers)
+        self.assertTrue(are_fb)
+    
