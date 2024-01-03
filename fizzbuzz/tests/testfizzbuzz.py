e142b438096ce31638c46a8d2597b6af0233b391
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-03 19:47:58 +0700

    update test case 5 pass 1

--- fizzbuzz/tests/testfizzbuzz.py
+++ fizzbuzz/tests/testfizzbuzz.py
@@ -5,4 +5,25 @@ class PrimeListTest(unittest.TestCase):
         numbers = [1, 2, 3]
         are_fb = FizzBuzz(numbers)
         self.assertTrue(are_fb)
+
+    def test_give_9 (self):
+        numbers = [9]
+        are_fb = FizzBuzz(numbers)
+        self.assertTrue(are_fb) 
+
+    def test_give_22_26_28 (self):
+        numbers = [22, 26, 28]
+        are_fb = FizzBuzz(numbers)
+        self.assertTrue(are_fb) 
+
+    def test_give_75_79_54 (self):
+        numbers = [75, 79, 54]
+        are_fb = FizzBuzz(numbers)
+        self.assertTrue(are_fb) 
+
+    def test_give_69_96_1024_51_113 (self):
+        numbers = [69, 96, 1024, 51, 113]
+        are_fb = FizzBuzz(numbers)
+        self.assertTrue(are_fb)          
+
     
