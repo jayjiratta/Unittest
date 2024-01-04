5229ad13a2b76992385cb5219a948dac61df94d6
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-04 21:50:25 +0700

    test case 16 case finish

--- fizzbuzz/tests/testfizzbuzz.py
+++ fizzbuzz/tests/testfizzbuzz.py
@@ -1,29 +1,83 @@
-from fizzbuzz.fizzbuzz_d import get_fizzbuzz
+from fizzbuzz.function.fizzbuzz_d import get_fizzbuzz
 import unittest
 class PrimeListTest(unittest.TestCase):
-    def test_give_1_is_empty_str (self): 
-        number = 1
+    def test_give_neg_2_is_empty_str (self): 
+        number = -2
         are_fb = get_fizzbuzz(number)
         self.assertEqual(are_fb,"")
 
-    def test_give_9_is_fizz (self): 
-        number = 9
+    def test_give_neg_201717_is_fizz (self): 
+        number = 201717
         are_fb = get_fizzbuzz(number)
         self.assertEqual(are_fb,'Fizz') 
 
-    def test_give_22_is_empty_str (self): 
-        number = 22
-        are_fb = get_fizzbuzz(number)
-        self.assertEqual(are_fb,"") 
-
-    def test_give_75_is_fizzbuzz (self): 
-        number = 75
+    def test_give_465_is_fizzbuzz (self): 
+        number = 465
         are_fb = get_fizzbuzz(number)
         self.assertEqual(are_fb,"FizzBuzz") 
 
-    def test_give_25_is_buzz (self): 
-        number = 25
+    def test_give_neg_1940_is_buzz (self): 
+        number = -1940
         are_fb = get_fizzbuzz(number)
-        self.assertEqual(are_fb,"Buzz")          
+        self.assertEqual(are_fb,"Buzz")  
+    
+    def test_give_neg_15_is_fizzbuzz (self): 
+        number = -15
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"FizzBuzz")         
 
+    def test_give_neg_137_is_empty_str (self): 
+        number = -137
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"")
+
+    def test_give_neg_1324_is_empty_str (self): 
+        number = -1324
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"")
+
+    def test_give_neg_80_is_buzz (self): 
+        number = -80
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"Buzz")
+    
+    def test_give_neg_723542_is_empty_str (self): 
+        number = -723542
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"")
+    
+    def test_give_750_is_fizzbuzz (self): 
+        number = 750
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"FizzBuzz")
+    
+    def test_give_14_is_empty_str (self): 
+        number = 14
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"")
+
+    def test_give_791_is_empty_str (self): 
+        number = 791
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"")
+    
+    def test_give_2478_is_fizz (self): 
+        number = 2478
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,'Fizz') 
+
+    def test_give_10569_is_fizz (self): 
+        number = 10569
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,'Fizz') 
+    
+    def test_give_545_is_buzz (self): 
+        number = 545
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"Buzz")  
+    
+    def test_give_960_is_fizzbuzz (self): 
+        number = 960
+        are_fb = get_fizzbuzz(number)
+        self.assertEqual(are_fb,"FizzBuzz")
     
